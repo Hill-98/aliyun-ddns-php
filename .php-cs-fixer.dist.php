ee51bf979f574d2978693d8c2926e530bd6e06fd
Zhong Lufan <lufanzhong@gmail.com>
2021-07-07 16:44:05 +0800

    php-cs-fixer: Exclude config file

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 const RISKY = true;
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__);
+    ->in(__DIR__)
+    ->notPath(['config.php', 'config.example.php']);
 
 return require __DIR__.'/vendor/hill-98/php-cs-fixer-config/main.php';
